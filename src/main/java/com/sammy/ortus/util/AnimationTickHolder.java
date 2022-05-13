42fb564f16851a951b7e7b266160325c84704d47
amo <lunarteclipse@gmail.com>
2022-05-13 19:58:20 +0100

    forgot me lighter render event

---
+++ src/main/java/com/sammy/ortus/util/AnimationTickHolder.java
@@ -0,0 +1,53 @@
+package com.sammy.ortus.util;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.LevelAccessor;
+
+public class AnimationTickHolder {
+
+    private static int ticks;
+    private static int pausedTicks;
+
+    public static void reset() {
+        ticks = 0;
+        pausedTicks = 0;
+    }
+
+    public static void tick() {
+        if (!Minecraft.getInstance()
+                .isPaused()) {
+            ticks = (ticks + 1) % 1_728_000; // wrap around every 24 hours so we maintain enough floating point precision
+        } else {
+            pausedTicks = (pausedTicks + 1) % 1_728_000;
+        }
+    }
+
+    public static int getTicks() {
+        return getTicks(false);
+    }
+
+    public static int getTicks(boolean includePaused) {
+        return includePaused ? ticks + pausedTicks : ticks;
+    }
+
+    public static float getRenderTime() {
+        return getTicks() + getPartialTicks();
+    }
+
+    public static float getPartialTicks() {
+        Minecraft mc = Minecraft.getInstance();
+        return (mc.isPaused() ? 1 : mc.getFrameTime());
+    }
+
+    public static int getTicks(LevelAccessor world) {
+        return getTicks();
+    }
+
+    public static float getRenderTime(LevelAccessor world) {
+        return getTicks(world) + getPartialTicks(world);
+    }
+
+    public static float getPartialTicks(LevelAccessor world) {
+        return getPartialTicks();
+    }
+}
