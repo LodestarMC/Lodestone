36c891faecd9b526b2b6a9835615836cc38459c3
amo <lunarteclipse@gmail.com>
2022-05-13 18:51:20 +0100

    forgot me lighter

---
+++ src/main/java/com/sammy/ortus/helpers/placement/util/PoleHelper.java
@@ -0,0 +1,79 @@
+package com.sammy.ortus.helpers.placement.util;
+
+import com.sammy.ortus.helpers.placement.IPlacementHelper;
+import com.sammy.ortus.helpers.placement.PlacementOffset;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.common.ForgeMod;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+@MethodsReturnNonnullByDefault
+public abstract class PoleHelper<T extends Comparable<T>> implements IPlacementHelper {
+
+    protected final Predicate<BlockState> statePredicate;
+    protected final Property<T> property;
+    protected final Function<BlockState, Direction.Axis> axisFunction;
+
+    public PoleHelper(Predicate<BlockState> statePredicate, Function<BlockState, Direction.Axis> axisFunction, Property<T> property) {
+        this.statePredicate = statePredicate;
+        this.axisFunction = axisFunction;
+        this.property = property;
+    }
+
+    public boolean matchesAxis(BlockState state, Direction.Axis axis) {
+        if (!statePredicate.test(state))
+            return false;
+
+        return axisFunction.apply(state) == axis;
+    }
+
+    public int attachedPoles(Level world, BlockPos pos, Direction direction) {
+        BlockPos checkPos = pos.relative(direction);
+        BlockState state = world.getBlockState(checkPos);
+        int count = 0;
+        while (matchesAxis(state, direction.getAxis())) {
+            count++;
+            checkPos = checkPos.relative(direction);
+            state = world.getBlockState(checkPos);
+        }
+        return count;
+    }
+
+    @Override
+    public Predicate<BlockState> getStatePredicate() {
+        return this.statePredicate;
+    }
+
+    @Override
+    public PlacementOffset getOffset(Player player, Level world, BlockState state, BlockPos pos, BlockHitResult ray) {
+        List<Direction> directions = IPlacementHelper.orderedByDistance(pos, ray.getLocation(), dir -> dir.getAxis() == axisFunction.apply(state));
+        for (Direction dir : directions) {
+            int range = 4;
+            if (player != null) {
+                AttributeInstance reach = player.getAttribute(ForgeMod.REACH_DISTANCE.get());
+            }
+            int poles = attachedPoles(world, pos, dir);
+            if (poles >= range)
+                continue;
+
+            BlockPos newPos = pos.relative(dir, poles + 1);
+            BlockState newState = world.getBlockState(newPos);
+
+            if (newState.getMaterial().isReplaceable())
+                return PlacementOffset.success(newPos, bState -> bState.setValue(property, state.getValue(property)));
+
+        }
+
+        return PlacementOffset.fail();
+    }
+}
