738c7a36983b790b432efeeb243ed85881b70886
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2022-05-20 16:05:01 +0200

    Fix the bug

--- src/main/java/com/sammy/ortus/mixin/LevelRendererMixin.java
+++ src/main/java/com/sammy/ortus/mixin/LevelRendererMixin.java
@@ -2,16 +2,21 @@ package com.sammy.ortus.mixin;
 
 import com.sammy.ortus.systems.sound.ExtendedSoundType;
 import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.SoundType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
 
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin {
@@ -25,8 +30,10 @@ public class LevelRendererMixin {
 
     @Inject(method = "levelEvent", slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;stateById(I)Lnet/minecraft/world/level/block/state/BlockState;")), at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;playLocalSound(Lnet/minecraft/core/BlockPos;Lnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FFZ)V", ordinal = 0))
     private void ortusCallExtendedBreakSound(Player pPlayer, int pType, BlockPos pPos, int pData, CallbackInfo ci) {
-        if (type instanceof ExtendedSoundType extendedSoundType) {
-            extendedSoundType.onPlayBreakSound(pPlayer.level, pPos);
+        if (pPlayer != null) {
+            if (type instanceof ExtendedSoundType extendedSoundType) {
+                extendedSoundType.onPlayBreakSound(pPlayer.level, pPos);
+            }
         }
     }
 }
