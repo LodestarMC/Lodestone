0054d036459d1683b783d444010bfbfe819914e7
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2022-06-16 22:14:51 +0200

    Add client & server nbt packets

---
+++ src/main/java/com/sammy/ortus/systems/network/OrtusClientNBTPacket.java
@@ -0,0 +1,30 @@
+package com.sammy.ortus.systems.network;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.network.NetworkEvent;
+
+import java.util.function.Supplier;
+
+public abstract class OrtusClientNBTPacket extends OrtusClientPacket {
+    protected CompoundTag data;
+
+    public OrtusClientNBTPacket(CompoundTag data) {
+        this.data = data;
+    }
+
+    public void encode(FriendlyByteBuf buf) {
+        buf.writeNbt(data);
+    }
+
+    @Override
+    public final void execute(Supplier<NetworkEvent.Context> context) {
+        execute(context, data);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public void execute(Supplier<NetworkEvent.Context> context, CompoundTag data) {
+    }
+}
