00afadfd24260aeec54628701e413728cec22a04
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2022-09-11 06:45:53 +0200

    Add dimension filters for features

--- src/main/java/team/lodestar/lodestone/setup/LodestonePlacementFillerRegistry.java
+++ src/main/java/team/lodestar/lodestone/setup/LodestonePlacementFillerRegistry.java
@@ -8,16 +8,19 @@ import net.minecraft.world.level.levelgen.placement.PlacementModifierType;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import team.lodestar.lodestone.systems.worldgen.DimensionPlacementFilter;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class LodestonePlacementFillerRegistry {
 
     public static PlacementModifierType<ChancePlacementFilter> CHANCE;
+    public static PlacementModifierType<DimensionPlacementFilter> DIMENSION;
 
     @SubscribeEvent
     public static void registerTypes(FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
             CHANCE = register("lodestone:chance", ChancePlacementFilter.CODEC);
+            DIMENSION = register("lodestone:dimension", DimensionPlacementFilter.CODEC);
         });
     }
 
---
+++ src/main/java/team/lodestar/lodestone/systems/worldgen/DimensionPlacementFilter.java
@@ -0,0 +1,50 @@
+package team.lodestar.lodestone.systems.worldgen;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.levelgen.placement.PlacementContext;
+import net.minecraft.world.level.levelgen.placement.PlacementFilter;
+import net.minecraft.world.level.levelgen.placement.PlacementModifierType;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class DimensionPlacementFilter extends PlacementFilter {
+  public static final Codec<DimensionPlacementFilter> CODEC = RecordCodecBuilder.create((codec) -> codec.group(
+      ResourceLocation.CODEC.listOf().fieldOf("dimensions").forGetter(o -> o.dimensions.stream().map(ResourceKey::location).collect(Collectors.toList()))).apply(codec, (r) -> new DimensionPlacementFilter(r.stream().map(o -> ResourceKey.create(Registry.DIMENSION_REGISTRY, o)).collect(Collectors.toSet()))));
+
+  private final Set<ResourceKey<Level>> dimensions;
+
+  protected DimensionPlacementFilter(Set<ResourceKey<Level>> dimensions) {
+    this.dimensions = dimensions;
+  }
+
+  public static DimensionPlacementFilter of(Set<ResourceKey<Level>> dimensions) {
+    return new DimensionPlacementFilter(dimensions);
+  }
+
+  @Override
+  protected boolean shouldPlace(PlacementContext p_191835_, Random p_191836_, BlockPos p_191837_) {
+    ServerLevel level = p_191835_.getLevel().getLevel();
+    return dimensions.contains(level.dimension());
+  }
+
+  @Override
+  public PlacementModifierType<?> type() {
+    return ModFeatures.DIMENSION_PLACEMENT.get();
+  }
+
+  public static class Type implements PlacementModifierType<DimensionPlacementFilter> {
+    @Override
+    public Codec<DimensionPlacementFilter> codec() {
+      return DimensionPlacementFilter.CODEC;
+    }
+  }
+}
