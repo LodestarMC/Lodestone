0ff277cc745a1fa615a622ddd28fccf394d5387a
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2023-02-10 09:51:26 +0100

    Update VFXBuilders.java

--- src/main/java/team/lodestar/lodestone/systems/rendering/VFXBuilders.java
+++ src/main/java/team/lodestar/lodestone/systems/rendering/VFXBuilders.java
@@ -267,9 +267,9 @@ public class VFXBuilders {
         public WorldVFXBuilder setPosColorTexLightmapDefaultFormat() {
             return setVertexSupplier((c, l, x, y, z, u, v) -> {
                 if (l == null)
-                    c.vertex(x, y, z).color(this.r, this.g, this.b, this.a).uv(u, v).uv2(this.light).endVertex();
+                    c.vertex(x, y, z).color(this.r, this.g, this.b, this.a).uv(u, v).uv2(this.light >> 16 & 0xFFFF, this.light & 0xFFFF).endVertex();
                 else
-                    c.vertex(l, x, y, z).color(this.r, this.g, this.b, this.a).uv(u, v).uv2(this.light).endVertex();
+                    c.vertex(l, x, y, z).color(this.r, this.g, this.b, this.a).uv(u, v).uv2(this.light >> 16 & 0xFFFF, this.light & 0xFFFF).endVertex();
             }).setFormat(DefaultVertexFormat.POSITION_COLOR_TEX_LIGHTMAP);
         }
 
