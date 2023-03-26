6f655ccd4c1d0eeff901f41d56756b8ba66b7379
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2023-03-26 18:18:02 +0200

    Update IPlacementAssistant.java

--- src/main/java/team/lodestar/lodestone/systems/placementassistance/IPlacementAssistant.java
+++ src/main/java/team/lodestar/lodestone/systems/placementassistance/IPlacementAssistant.java
@@ -1,19 +1,15 @@
 package team.lodestar.lodestone.systems.placementassistance;
 
-import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
 import java.util.function.Predicate;
 
 /**
- * A placement assistant is a helpful client-sided system which allows you to do custom things when a player places something.
+ * A placement assistant is a helpful client-sided system which allows you to create systems that preview unique block placement mechanics.
  */
 public interface IPlacementAssistant {
 
