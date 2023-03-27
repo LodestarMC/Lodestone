090353103703f055645271895e846c3cbde670d9
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2023-03-27 19:14:51 +0200

    Update BlockStateSmithTypes.java

--- src/main/java/team/lodestar/lodestone/systems/datagen/BlockStateSmithTypes.java
+++ src/main/java/team/lodestar/lodestone/systems/datagen/BlockStateSmithTypes.java
@@ -10,36 +10,64 @@ import team.lodestar.lodestone.systems.datagen.statesmith.ModularBlockStateSmith
 
 public class BlockStateSmithTypes {
 
+    /**
+     * A modular state smith, requires two separate functions, one that generates the model, and another that creates the blockstate.
+     */
     public static ModularBlockStateSmith<Block> CUSTOM_MODEL = new ModularBlockStateSmith<>(Block.class, (block, provider, stateFunction, modelFileSupplier) -> {
         stateFunction.act(block, modelFileSupplier.generateModel(block));
     });
 
+    /**
+     * Generates a cube model and a blockstate to match.
+     */
     public static BlockStateSmith<Block> FULL_BLOCK = new BlockStateSmith<>(Block.class, (block, provider) -> {
         provider.simpleBlock(block);
     });
 
+    /**
+     * Generates a cross model, used by flowers and grass, and a blockstate to match.
+     */
     public static BlockStateSmith<Block> CROSS_MODEL_BLOCK = new BlockStateSmith<>(Block.class, (block, provider) -> {
         String name = provider.getBlockName(block);
         provider.models().cross(name, provider.getBlockTexture(name));
     });
 
+    /**
+     * Generates leaves. Not much else to say here.
+     */
     public static BlockStateSmith<Block> LEAVES_BLOCK = new BlockStateSmith<>(Block.class, (block, provider) -> {
         String name = provider.getBlockName(block);
         ModelFile leaves = provider.models().withExistingParent(name, new ResourceLocation("block/leaves")).texture("all", provider.getBlockTexture(name));
         provider.simpleBlock(block, leaves);
     });
 
+    /**
+     * Generates a log block model and state, or any pillar-like block really.
+     */
     public static BlockStateSmith<RotatedPillarBlock> LOG_BLOCK = new BlockStateSmith<>(RotatedPillarBlock.class, (block, provider) -> {
         provider.logBlock(block);
     });
 
+    /**
+     * Generates an axis oriented block model and state.
+     */
+    public static BlockStateSmith<RotatedPillarBlock> AXIS_BLOCK = new BlockStateSmith<>(RotatedPillarBlock.class, (block, provider) -> {
+        provider.axisBlock(block);
+    });
+
+    /**
+     * Generates a wood block model and state, one of those 6-sided log blocks mainly used for building.
+     */
     public static BlockStateSmith<RotatedPillarBlock> WOOD_BLOCK = new BlockStateSmith<>(RotatedPillarBlock.class, (block, provider) -> {
         String name = provider.getBlockName(block);
-        String textureName = name + "_log";
+        String textureName = name.replace("_wood", "") + "_log";
         ResourceLocation logTexture = provider.getBlockTexture(textureName);
         provider.axisBlock(block, logTexture, logTexture);
     });
 
+    /**
+     * Generates a directional block model and state.
+     */
     public static BlockStateSmith<DirectionalBlock> DIRECTIONAL_BLOCK = new BlockStateSmith<>(DirectionalBlock.class, (block, provider) -> {
         String name = provider.getBlockName(block);
         ResourceLocation textureName = provider.getBlockTexture(name);
@@ -47,64 +75,97 @@ public class BlockStateSmithTypes {
         provider.directionalBlock(block, directionalModel);
     });
 
+    /**
+     * Generates stairs!
+     */
     public static BlockStateSmith<StairBlock> STAIRS_BLOCK = new BlockStateSmith<>(StairBlock.class, (block, provider) -> {
         String name = provider.getBlockName(block);
         String textureName = name.replace("_stairs", "");
         provider.stairsBlock(block, provider.getBlockTexture(textureName));
     });
 
+    /**
+     * Slabs!
+     */
     public static BlockStateSmith<SlabBlock> SLAB_BLOCK = new BlockStateSmith<>(SlabBlock.class, (block, provider) -> {
         String name = provider.getBlockName(block);
         String textureName = name.replace("_slab", "");
         provider.slabBlock(block, provider.getBlockTexture(textureName), provider.getBlockTexture(textureName));
     });
 
+    /**
+     * Generates a wall block model and state.
+     */
     public static BlockStateSmith<WallBlock> WALL_BLOCK = new BlockStateSmith<>(WallBlock.class, (block, provider) -> {
         String name = provider.getBlockName(block);
         String textureName = name.replace("_wall", "");
         provider.wallBlock(block, provider.getBlockTexture(textureName));
     });
 
+    /**
+     * Generates a fence block model and state.
+     */
     public static BlockStateSmith<FenceBlock> FENCE_BLOCK = new BlockStateSmith<>(FenceBlock.class, (block, provider) -> {
         String name = provider.getBlockName(block);
         String textureName = name.replace("_fence", "");
         provider.fenceBlock(block, provider.getBlockTexture(textureName));
     });
 
+    /**
+     * Generates a fence gate block model and state.
+     */
     public static BlockStateSmith<FenceGateBlock> FENCE_GATE_BLOCK = new BlockStateSmith<>(FenceGateBlock.class, (block, provider) -> {
         String name = provider.getBlockName(block);
         String textureName = name.replace("_fence_gate", "");
         provider.fenceGateBlock(block, provider.getBlockTexture(textureName));
     });
 
+    /**
+     * Generates a pressure plate block model and state.
+     */
     public static BlockStateSmith<PressurePlateBlock> PRESSURE_PLATE_BLOCK = new BlockStateSmith<>(PressurePlateBlock.class, (block, provider) -> {
         String name = provider.getBlockName(block);
         String textureName = name.replace("_pressure_plate", "");
         provider.pressurePlateBlock(block, provider.getBlockTexture(textureName));
     });
 
+    /**
+     * Generates a button block model and state.
+     */
     public static BlockStateSmith<ButtonBlock> BUTTON_BLOCK = new BlockStateSmith<>(ButtonBlock.class, (block, provider) -> {
         String name = provider.getBlockName(block);
         String textureName = name.replace("_button", "");
         provider.buttonBlock(block, provider.getBlockTexture(textureName));
     });
 
+    /**
+     * DOOR.
+     */
     public static BlockStateSmith<DoorBlock> DOOR_BLOCK = new BlockStateSmith<>(DoorBlock.class, (block, provider) -> {
         String name = provider.getBlockName(block);
         provider.doorBlock(block, provider.getBlockTexture(name + "_bottom"), provider.getBlockTexture(name + "_top"));
     });
 
+    /**
+     * Generates a trapdoor block model and state.
+     */
     public static BlockStateSmith<TrapDoorBlock> TRAPDOOR_BLOCK = new BlockStateSmith<>(TrapDoorBlock.class, (block, provider) -> {
         String name = provider.getBlockName(block);
         provider.trapdoorBlock(block, provider.getBlockTexture(name), true);
     });
 
+    /**
+     * Generates a standing torch block model and state. Wall torch not included.
+     */
     public static BlockStateSmith<TorchBlock> TORCH_BLOCK = new BlockStateSmith<>(TorchBlock.class, (block, provider) -> {
         String name = provider.getBlockName(block);
         ModelFile torchModel = provider.models().torch(provider.getBlockName(block), provider.getBlockTexture(name));
         provider.getVariantBuilder(block).forAllStates(s -> ConfiguredModel.builder().modelFile(torchModel).build());
     });
 
+    /**
+     * Generates a wall torch block model and state. Standing torch not included.
+     */
     public static BlockStateSmith<WallTorchBlock> WALL_TORCH_BLOCK = new BlockStateSmith<>(WallTorchBlock.class, (block, provider) -> {
         String name = provider.getBlockName(block);
         String textureName = name.replace("wall_", "");
@@ -112,6 +173,9 @@ public class BlockStateSmithTypes {
         provider.horizontalBlock(block, torchModel);
     });
 
+    /**
+     * Generates a wooden sign block model and state. Works for both standing, and wall oriented signs.
+     */
     public static BlockStateSmith<SignBlock> WOODEN_SIGN_BLOCK = new BlockStateSmith<>(SignBlock.class, (block, provider) -> {
         String name = provider.getBlockName(block);
         String particleTextureName = name.replace("_wall", "").replace("_sign", "") + "_planks";
