4201bd05e678cf71efce3904e81041c56dd3c2e0
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2023-03-27 13:17:23 +0200

    Create LodestoneHorizontalBlock.java

---
+++ src/main/java/team/lodestar/lodestone/systems/block/LodestoneHorizontalBlock.java
@@ -0,0 +1,22 @@
+package team.lodestar.lodestone.systems.block;
+
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+
+public class LodestoneHorizontalBlock extends HorizontalDirectionalBlock {
+    public LodestoneHorizontalBlock(Properties builder) {
+        super(builder);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+}
