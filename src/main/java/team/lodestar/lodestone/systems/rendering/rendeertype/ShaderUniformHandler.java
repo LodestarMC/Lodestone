4e1c348083172818b5f330b616a45cda72281e86
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2023-06-25 00:40:12 +0200

    Update ShaderUniformHandler.java

--- src/main/java/team/lodestar/lodestone/systems/rendering/rendeertype/ShaderUniformHandler.java
+++ src/main/java/team/lodestar/lodestone/systems/rendering/rendeertype/ShaderUniformHandler.java
@@ -5,7 +5,7 @@ import net.minecraft.client.renderer.ShaderInstance;
 public interface ShaderUniformHandler {
 
     ShaderUniformHandler LUMITRANSPARENT = instance -> {
-        instance.safeGetUniform("LumiTransparency").set(1);
+        instance.safeGetUniform("LumiTransparency").set(1f);
     };
 
     void updateShaderData(ShaderInstance instance);
