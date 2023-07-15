eb53a39ed6e3c5174d5216281358a593041544bd
EpicSquid <epicsquid315@gmail.com>
2023-07-15 16:11:48 +1000

    Fixed mixins

--- src/main/java/team/lodestar/lodestone/mixin/ScreenMixin.java
+++ src/main/java/team/lodestar/lodestone/mixin/ScreenMixin.java
@@ -1,19 +1,19 @@
 package team.lodestar.lodestone.mixin;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import team.lodestar.lodestone.handlers.screenparticle.ScreenParticleHandler;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import team.lodestar.lodestone.handlers.screenparticle.ScreenParticleHandler;
 
 
 @Mixin(Screen.class)
 public class ScreenMixin {
 
-    @Inject(at = @At("HEAD"), method = "renderBackground(Lcom/mojang/blaze3d/vertex/PoseStack;I)V")
-    private void lodestoneBeforeUiParticleMixin(PoseStack pPoseStack, int pVOffset, CallbackInfo ci) {
+    @Inject(at = @At("HEAD"), method = "renderBackground(Lnet/minecraft/client/gui/GuiGraphics;)V")
+    private void lodestoneBeforeUiParticleMixin(GuiGraphics graphics, CallbackInfo ci) {
         ScreenParticleHandler.renderEarliestParticles();
     }
 }
