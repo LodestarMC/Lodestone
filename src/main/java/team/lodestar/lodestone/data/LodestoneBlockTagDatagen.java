2e98bc4a1a5860bdc98affe42933283cfe418e69
EpicSquid <epicsquid315@gmail.com>
2023-07-15 10:03:02 +1000

    Updated tag generation constructors

--- src/main/java/team/lodestar/lodestone/data/LodestoneBlockTagDatagen.java
+++ src/main/java/team/lodestar/lodestone/data/LodestoneBlockTagDatagen.java
@@ -1,22 +1,28 @@
 package team.lodestar.lodestone.data;
 
-import team.lodestar.lodestone.LodestoneLib;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
 
 public class LodestoneBlockTagDatagen extends BlockTagsProvider {
-    public LodestoneBlockTagDatagen(DataGenerator generatorIn, ExistingFileHelper existingFileHelper) {
-        super(generatorIn, LodestoneLib.LODESTONE, existingFileHelper);
-    }
 
-    @Override
-    public String getName() {
-        return "Lodestone Block Tags";
-    }
+	public LodestoneBlockTagDatagen(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, String modId, @Nullable ExistingFileHelper existingFileHelper) {
+		super(output, lookupProvider, modId, existingFileHelper);
+	}
 
-    @Override
-    protected void addTags() {
+	@Override
+	@NotNull
+	public String getName() {
+		return "Lodestone Block Tags";
+	}
 
-    }
+	@Override
+	protected void addTags(HolderLookup.@NotNull Provider provider) {
+
+	}
 }
