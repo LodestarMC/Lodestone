5d888a1d0345ec07b7042aa747614bd578b98368
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2023-09-21 16:29:26 +0200

    wawawawawawawa

--- src/main/java/team/lodestar/lodestone/systems/particle/data/ColorParticleData.java
+++ src/main/java/team/lodestar/lodestone/systems/particle/data/ColorParticleData.java
@@ -26,8 +26,8 @@ public class ColorParticleData {
         return Mth.clamp((age * colorCoefficient) / lifetime, 0, 1);
     }
 
-    public static ColorParticleDataBuilder create(ColorParticleData data) {
-        return create(data.r1, data.g1, data.b1, data.r2, data.g2, data.b2).setCoefficient(data.colorCoefficient).setEasing(data.colorCurveEasing);
+    public ColorParticleDataBuilder copy() {
+        return create(r1, g1, b1, r2, g2, b2).setCoefficient(colorCoefficient).setEasing(colorCurveEasing);
     }
 
     public static ColorParticleDataBuilder create(float r1, float g1, float b1, float r2, float g2, float b2) {
