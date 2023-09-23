527e1eb16e8db9171b86b8f71159f7ec45715bb0
mrsterner <simon@sterner.dev>
2023-09-23 18:55:30 +0200

    merg

--- src/main/java/team/lodestar/lodestone/LodestoneLib.java
+++ src/main/java/team/lodestar/lodestone/LodestoneLib.java
@@ -29,9 +29,9 @@ import team.lodestar.lodestone.handlers.screenparticle.ParticleEmitterHandler;
 import team.lodestar.lodestone.setup.*;
 import team.lodestar.lodestone.systems.easing.Easing;
 import team.lodestar.lodestone.systems.particle.builder.ScreenParticleBuilder;
-import team.lodestar.lodestone.systems.particle.data.ColorParticleData;
 import team.lodestar.lodestone.systems.particle.data.GenericParticleData;
-import team.lodestar.lodestone.systems.particle.data.SpinParticleData;
+import team.lodestar.lodestone.systems.particle.data.color.ColorParticleData;
+import team.lodestar.lodestone.systems.particle.data.spin.SpinParticleData;
 import team.lodestar.lodestone.systems.particle.screen.ScreenParticleHolder;
 
 import java.awt.*;
@@ -64,30 +64,7 @@ public class LodestoneLib {
 
         @Override
         public void spawnLateParticles(ScreenParticleHolder target, Level level, float partialTick, ItemStack stack, float x, float y) {
-            spawnParticles(target, level, partialTick, stack, x, y);
-        }
 
-        public void spawnParticles(ScreenParticleHolder target, Level level, float partialTick, ItemStack stack, float x, float y) {
-            float time = level.getGameTime() + partialTick;
-
-            Color firstColor = new Color(15712278);
-            Color secondColor = new Color(4607909);
-            float alphaMultiplier = 0.5f;
-            final int yOffset = 0;
-            final int xOffset = 0;
-            final SpinParticleData.SpinParticleDataBuilder spinDataBuilder = SpinParticleData.create(0, 1).setSpinOffset(0.025f * time % 6.28f).setEasing(Easing.EXPO_IN_OUT);
-            ScreenParticleBuilder.create(LodestoneScreenParticleRegistry.STAR, target)
-                    .setTransparencyData(GenericParticleData.create(0.09f * alphaMultiplier, 0f).setEasing(Easing.QUINTIC_IN).build())
-                    .setScaleData(GenericParticleData.create((float) (1.5f + Math.sin(time * 0.1f) * 0.125f), 0).build())
-                    .setColorData(ColorParticleData.create(firstColor, secondColor).setCoefficient(1.25f).build())
-                    .setLifetime(6)
-                    .setRandomOffset(0.05f)
-                    .setSpinData(spinDataBuilder.build())
-                    .spawnOnStack(xOffset, yOffset)
-                    .setScaleData(GenericParticleData.create((float) (1.4f - Math.sin(time * 0.075f) * 0.125f), 0).build())
-                    .setColorData(ColorParticleData.create(secondColor, firstColor).build())
-                    .setSpinData(spinDataBuilder.setSpinOffset(0.785f - 0.01f * time % 6.28f).build())
-                    .spawnOnStack(xOffset, yOffset);
         }
     }
 
