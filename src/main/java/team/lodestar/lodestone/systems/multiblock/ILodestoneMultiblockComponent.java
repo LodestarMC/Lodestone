035fd5c67136d362220a48714aaedf4986e4d17b
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2023-12-05 16:22:01 +0100

    Update ILodestoneMultiblockComponent.java

--- src/main/java/team/lodestar/lodestone/systems/multiblock/ILodestoneMultiblockComponent.java
+++ src/main/java/team/lodestar/lodestone/systems/multiblock/ILodestoneMultiblockComponent.java
@@ -2,7 +2,7 @@ package team.lodestar.lodestone.systems.multiblock;
 
 
 /**
- * Blocks that implement this interface will automatically be valid targets for the {@link team.lodestar.lodestone.setup.LodestoneBlockEntityRegistry#MULTIBLOCK_COMPONENT} block entity.
+ * Blocks that implement this interface will automatically be valid targets for the {@link team.lodestar.lodestone.registry.common.LodestoneBlockEntityRegistry#MULTIBLOCK_COMPONENT} block entity.
  */
 public interface ILodestoneMultiblockComponent {
 }
