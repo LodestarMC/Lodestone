6579a43cc42cd311bf3659095979f27347e8fcd5
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2024-05-25 22:14:41 +0200

    Update RenderTypeToken stuff

--- src/main/java/team/lodestar/lodestone/systems/rendering/rendeertype/RenderTypeToken.java
+++ src/main/java/team/lodestar/lodestone/systems/rendering/rendeertype/RenderTypeToken.java
@@ -3,24 +3,31 @@ package team.lodestar.lodestone.systems.rendering.rendeertype;
 import net.minecraft.client.renderer.*;
 import net.minecraft.resources.*;
 
+import java.util.*;
 import java.util.function.*;
 
 public class RenderTypeToken implements Supplier<RenderStateShard.EmptyTextureStateShard> {
 
-    private final String identifier;
+    private static final HashMap<ResourceLocation, RenderTypeToken> CACHED_TOKENS = new HashMap<>();
+
+    private final UUID identifier;
     private final RenderStateShard.EmptyTextureStateShard texture;
 
-    public RenderTypeToken(String identifier, ResourceLocation texture) {
-        this(identifier, texture, false, false);
+    protected RenderTypeToken(ResourceLocation texture) {
+        this(new RenderStateShard.TextureStateShard(texture, false, false));
     }
 
-    public RenderTypeToken(String identifier, ResourceLocation texture, boolean blur, boolean mipmap) {
-        this(identifier, new RenderStateShard.TextureStateShard(texture, blur, mipmap));
-    }
-
-    public RenderTypeToken(String identifier, RenderStateShard.EmptyTextureStateShard texture) {
-        this.identifier = identifier;
+    protected RenderTypeToken(RenderStateShard.EmptyTextureStateShard texture) {
+        this.identifier = UUID.randomUUID();
         this.texture = texture;
+    }
+
+    public static RenderTypeToken createToken(ResourceLocation texture) {
+        return new RenderTypeToken(texture);
+    }
+
+    public static RenderTypeToken createCachedToken(ResourceLocation texture) {
+        return CACHED_TOKENS.computeIfAbsent(texture, RenderTypeToken::new);
     }
 
     @Override
@@ -28,7 +35,8 @@ public class RenderTypeToken implements Supplier<RenderStateShard.EmptyTextureSt
         return texture;
     }
 
-    public String getIdentifier() {
-        return identifier;
+    @Override
+    public int hashCode() {
+        return Objects.hash(identifier, texture);
     }
 }
