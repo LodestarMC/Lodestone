c0e75078b54ffc37bd66c9b68c51b0628d015bc0
Cryptic <117136390+Its-Cryptic@users.noreply.github.com>
2024-05-26 03:30:21 -0700

    Added more buttons to chat gui
    Cleaned things up a bit and added a few more buttons to GUI.

--- src/main/java/team/lodestar/lodestone/command/ListActiveWorldEventsCommand.java
+++ src/main/java/team/lodestar/lodestone/command/ListActiveWorldEventsCommand.java
@@ -6,6 +6,7 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.TagTypes;
 import net.minecraft.network.chat.*;
 import net.minecraft.server.level.ServerLevel;
 import team.lodestar.lodestone.command.arguments.WorldEventTypeArgument;
@@ -95,44 +96,113 @@ public class ListActiveWorldEventsCommand {
 
             for (int i = startIndex; i < endIndex; i++) {
                 WorldEventInstance event = activeWorldEvents.get(i);
+
                 MutableComponent eventComponent = Component.literal(String.format("%02d. | %s | ", i + 1, event.type.id.toString()))
                         .withStyle(Style.EMPTY.withColor(i % 2 == 1 ? ChatFormatting.GRAY : ChatFormatting.WHITE)
-                                .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal(event.toString()))))
-                        .append(Component.literal("[>]").withStyle(Style.EMPTY.withColor(ChatFormatting.GREEN)
-                                .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent get " + event.uuid.toString()))
-                                .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Click to see details")))));
-                consumer.accept(eventComponent);
+                                .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal(event.toString()))));
+
+                MutableComponent detailsButton = Component.literal("[>]").withStyle(Style.EMPTY
+                        .withColor(ChatFormatting.GREEN)
+                        .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent get " + event.uuid.toString()))
+                        .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Click to see details"))));
+
+                MutableComponent freezeButton = Component.literal("[F]").withStyle(Style.EMPTY
+                        .withColor(ChatFormatting.AQUA)
+                        .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent freeze single " + event.uuid.toString()))
+                        .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Click to freeze"))));
+
+                MutableComponent unfreezeButton = Component.literal("[U]").withStyle(Style.EMPTY
+                        .withColor(ChatFormatting.DARK_AQUA)
+                        .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent unfreeze single " + event.uuid.toString()))
+                        .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Click to unfreeze"))));
+
+                MutableComponent removeButton = Component.literal("[X]").withStyle(Style.EMPTY
+                        .withColor(ChatFormatting.RED)
+                        .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent remove single " + event.uuid.toString()))
+                        .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Click to remove"))));
+
+                MutableComponent space = Component.literal("  ");
+
+                consumer.accept(eventComponent.append(space).append(detailsButton).append(space).append(event.isFrozen() ? unfreezeButton : freezeButton).append(space).append(removeButton));
             }
         }
 
         protected void buildInstanceDetailsPage(Consumer<Component> consumer, WorldEventInstance worldEventInstance) {
+            MutableComponent worldEventDetailsHeader = Component.literal("World Event Details:").withStyle(Style.EMPTY.withColor(ChatFormatting.GOLD).withBold(true));
+
+            MutableComponent worldEventUUID = Component.literal("UUID: ").withStyle(Style.EMPTY.withColor(ChatFormatting.GOLD))
+                    .append(Component.literal(worldEventInstance.uuid.toString()).withStyle(Style.EMPTY
+                            .withColor(ChatFormatting.GREEN)
+                            .withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, worldEventInstance.uuid.toString()))
+                            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Click to copy")))
+                    ));
+            MutableComponent worldEventType = Component.literal("Type: ").withStyle(Style.EMPTY.withColor(ChatFormatting.GOLD))
+                    .append(Component.literal(worldEventInstance.type.id.toString()).withStyle(Style.EMPTY
+                            .withColor(ChatFormatting.GREEN)
+                            .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent list type " + worldEventInstance.type.id.toString()))
+                            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Click to filter by type")))
+                    ));
+
+            MutableComponent frozenStatus = Component.literal("Frozen: ").withStyle(Style.EMPTY.withColor(ChatFormatting.GOLD))
+                    .append(Component.literal(worldEventInstance.isFrozen() ? "Yes" : "No").withStyle(Style.EMPTY
+                            .withColor(worldEventInstance.isFrozen() ? ChatFormatting.DARK_AQUA : ChatFormatting.GREEN)
+                    ));
+            MutableComponent worldEventData = Component.literal("Data:").withStyle(Style.EMPTY.withColor(ChatFormatting.GOLD));
+
             buildWhitespace(consumer);
             consumer.accept(getDashedLine().withStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)));
-            CompoundTag tag = worldEventInstance.serializeNBT(new CompoundTag());
-            consumer.accept(Component.literal("World Event Details:").withStyle(Style.EMPTY.withColor(ChatFormatting.GOLD).withBold(true)));
-            consumer.accept(Component.literal("UUID: ").append('"'+worldEventInstance.uuid.toString()+'"').withStyle(Style.EMPTY.withColor(ChatFormatting.GREEN)
-                    .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Copy UUID to clipboard")))
-                    .withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, worldEventInstance.uuid.toString()))));
-            consumer.accept(Component.literal("Type: ").append('"'+worldEventInstance.type.id.toString()+'"').withStyle(Style.EMPTY.withColor(ChatFormatting.GREEN)
-                    .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal(worldEventInstance.type.toString())))));
-
+            consumer.accept(worldEventDetailsHeader);
+            consumer.accept(worldEventUUID);
+            consumer.accept(worldEventType);
+            consumer.accept(frozenStatus);
             consumer.accept(getDashedSpaceLine().withStyle(Style.EMPTY.withColor(ChatFormatting.YELLOW)));
+            consumer.accept(worldEventData);
+            buildDetailsBody(consumer, worldEventInstance.serializeNBT(new CompoundTag()));
+            consumer.accept(getDashedLine().withStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)));
+            buildInteractiveFooter(consumer, worldEventInstance);
+        }
 
-            consumer.accept(Component.literal("Data:").withStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)));
+        private void buildDetailsBody(Consumer<Component> consumer, CompoundTag tag) {
             tag.tags.forEach((key, value) -> {
-                if (key.equals("uuid") || key.equals("type")) return;
+                if (key.equals("uuid") || key.equals("type") || key.equals("discarded") || key.equals("frozen")) return;
                 MutableComponent keyComponent = Component.literal(key + ": ").withStyle(Style.EMPTY.withColor(ChatFormatting.GREEN));
                 MutableComponent valueComponent = Component.literal(value.toString()).withStyle(Style.EMPTY.withColor(ChatFormatting.WHITE));
                 consumer.accept(keyComponent.append(valueComponent));
             });
-            // Back button
+        }
+
+        private void buildInteractiveFooter(Consumer<Component> consumer, WorldEventInstance instance) {
             MutableComponent backButton = Component.literal("[Back]")
                     .withStyle(Style.EMPTY
                             .withColor(ChatFormatting.GREEN)
                             .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent list page " + currentPage))
                             .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Back to World Event List"))));
-            consumer.accept(getDashedLine().withStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)));
-            consumer.accept(backButton);
+
+            MutableComponent freezeButton = Component.literal("[Freeze]")
+                    .withStyle(Style.EMPTY
+                            .withColor(ChatFormatting.DARK_BLUE)
+                            .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent freeze single " + instance.uuid.toString()))
+                            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Freeze this World Event"))));
+
+            MutableComponent unfreezeButton = Component.literal("[Unfreeze]")
+                    .withStyle(Style.EMPTY
+                            .withColor(ChatFormatting.DARK_AQUA)
+                            .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent unfreeze single " + instance.uuid.toString()))
+                            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Unfreeze this World Event"))));
+
+            MutableComponent removeButton = Component.literal("[Remove]")
+                    .withStyle(Style.EMPTY
+                            .withColor(ChatFormatting.RED)
+                            .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent remove single " + instance.uuid.toString()))
+                            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Remove this World Event"))));
+
+            MutableComponent reloadButton = Component.literal("[Reload]")
+                    .withStyle(Style.EMPTY
+                            .withColor(ChatFormatting.YELLOW)
+                            .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent get " + instance.uuid.toString()))
+                            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Reload this World Event"))));
+
+            consumer.accept(backButton.append("  ").append(instance.isFrozen() ? unfreezeButton : freezeButton).append("  ").append(removeButton).append("  ").append(reloadButton));
         }
 
         private void buildHeader(Consumer<Component> consumer) {
@@ -157,7 +227,34 @@ public class ListActiveWorldEventsCommand {
                             .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent list page " + (page + 1)))
                             .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Next Page"))));
 
-            consumer.accept(previousPage.append("  " + page + "  ").append(nextPage));
+            MutableComponent removeAll = Component.literal("[Remove All]")
+                    .withStyle(Style.EMPTY
+                            .withColor(ChatFormatting.RED)
+                            .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent remove all"))
+                            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Remove all active World Events"))));
+
+            MutableComponent freezeAll = Component.literal("[Freeze All]")
+                    .withStyle(Style.EMPTY
+                            .withColor(ChatFormatting.DARK_BLUE)
+                            .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent freeze all"))
+                            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Freeze all active World Events"))));
+
+            MutableComponent unfreezeAll = Component.literal("[Unfreeze All]")
+                    .withStyle(Style.EMPTY
+                            .withColor(ChatFormatting.DARK_AQUA)
+                            .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent unfreeze all"))
+                            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Unfreeze all active World Events"))));
+
+            MutableComponent reload = Component.literal("[Reload]")
+                    .withStyle(Style.EMPTY
+                            .withColor(ChatFormatting.YELLOW)
+                            .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lodestone worldevent list page " + page))
+                            .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Reload active World Events"))));
+
+            MutableComponent pageNumber = Component.literal(String.valueOf(page)).withStyle(Style.EMPTY.withColor(ChatFormatting.GOLD));
+            MutableComponent navigation = previousPage.append("  ").append(pageNumber).append("  ").append(nextPage);
+            MutableComponent freezeUnfreeze = frozenCount == instanceCount ? unfreezeAll : freezeAll;
+            consumer.accept(navigation.append("  ").append(freezeUnfreeze).append("  ").append(removeAll).append("  ").append(reload));
         }
 
         private void buildWhitespace(Consumer<Component> consumer) {
