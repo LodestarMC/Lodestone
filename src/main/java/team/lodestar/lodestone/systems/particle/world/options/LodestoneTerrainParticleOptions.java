82320e8b3b955a0c6f5050f0fcd599ae21b5bd74
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2024-05-29 03:07:27 +0200

    Use Supplier instead of raw type

--- src/main/java/team/lodestar/lodestone/systems/particle/world/options/LodestoneTerrainParticleOptions.java
+++ src/main/java/team/lodestar/lodestone/systems/particle/world/options/LodestoneTerrainParticleOptions.java
@@ -5,26 +5,28 @@ import net.minecraft.core.particles.ParticleType;
 import net.minecraft.world.level.block.state.BlockState;
 import team.lodestar.lodestone.systems.particle.world.behaviors.*;
 
+import java.util.function.*;
+
 public class LodestoneTerrainParticleOptions<T extends LodestoneParticleBehavior<T>> extends WorldParticleOptions<T> {
 
     public final BlockState blockState;
     public final BlockPos blockPos;
 
-    public LodestoneTerrainParticleOptions(ParticleType<?> type, T behavior, BlockState blockState, BlockPos blockPos) {
+    public LodestoneTerrainParticleOptions(Supplier<ParticleType<?>> type, T behavior, BlockState blockState, BlockPos blockPos) {
         super(type, behavior);
         this.blockState = blockState;
         this.blockPos = blockPos;
     }
 
-    public LodestoneTerrainParticleOptions(ParticleType<?> type, BlockState blockState, BlockPos blockPos) {
+    public LodestoneTerrainParticleOptions(Supplier<ParticleType<?>> type, BlockState blockState, BlockPos blockPos) {
         this(type, null, blockState, blockPos);
     }
 
-    public LodestoneTerrainParticleOptions(ParticleType<?> type, T behavior, BlockState blockState) {
+    public LodestoneTerrainParticleOptions(Supplier<ParticleType<?>> type, T behavior, BlockState blockState) {
         this(type, behavior, blockState, null);
     }
 
-    public LodestoneTerrainParticleOptions(ParticleType<?> type, BlockState blockState) {
+    public LodestoneTerrainParticleOptions(Supplier<ParticleType<?>> type, BlockState blockState) {
         this(type, blockState, null);
     }
 }
