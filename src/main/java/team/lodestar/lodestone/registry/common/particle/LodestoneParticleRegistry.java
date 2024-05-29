752cfa2f0707c891c385fa0753e68fd7de493420
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2024-05-29 02:41:57 +0200

    Cleanup

--- src/main/java/team/lodestar/lodestone/registry/common/particle/LodestoneParticleRegistry.java
+++ src/main/java/team/lodestar/lodestone/registry/common/particle/LodestoneParticleRegistry.java
@@ -8,7 +8,6 @@ import net.minecraftforge.registries.RegistryObject;
 import team.lodestar.lodestone.LodestoneLib;
 import team.lodestar.lodestone.systems.particle.world.type.LodestoneItemCrumbsParticleType;
 import team.lodestar.lodestone.systems.particle.world.type.LodestoneWorldParticleType;
-import team.lodestar.lodestone.systems.particle.world.type.LodestoneSparkParticleType;
 import team.lodestar.lodestone.systems.particle.world.type.LodestoneTerrainParticleType;
 
 @SuppressWarnings("unused")
@@ -20,8 +19,7 @@ public class LodestoneParticleRegistry {
     public static RegistryObject<LodestoneWorldParticleType> SPARKLE_PARTICLE = PARTICLES.register("sparkle", LodestoneWorldParticleType::new);
     public static RegistryObject<LodestoneWorldParticleType> TWINKLE_PARTICLE = PARTICLES.register("twinkle", LodestoneWorldParticleType::new);
     public static RegistryObject<LodestoneWorldParticleType> STAR_PARTICLE = PARTICLES.register("star", LodestoneWorldParticleType::new);
-
-    public static RegistryObject<LodestoneSparkParticleType> SPARK_PARTICLE = PARTICLES.register("spark", LodestoneSparkParticleType::new);
+    public static RegistryObject<LodestoneWorldParticleType> SPARK_PARTICLE = PARTICLES.register("spark", LodestoneWorldParticleType::new);
 
     public static RegistryObject<LodestoneTerrainParticleType> TERRAIN_PARTICLE = PARTICLES.register("terrain", LodestoneTerrainParticleType::new);
     public static RegistryObject<LodestoneItemCrumbsParticleType> ITEM_PARTICLE = PARTICLES.register("item", LodestoneItemCrumbsParticleType::new);
@@ -33,7 +31,7 @@ public class LodestoneParticleRegistry {
         event.registerSpriteSet(TWINKLE_PARTICLE.get(), LodestoneWorldParticleType.Factory::new);
         event.registerSpriteSet(STAR_PARTICLE.get(), LodestoneWorldParticleType.Factory::new);
 
-        event.registerSpriteSet(SPARK_PARTICLE.get(), LodestoneSparkParticleType.Factory::new);
+        event.registerSpriteSet(SPARK_PARTICLE.get(), LodestoneWorldParticleType.Factory::new);
         event.registerSpriteSet(TERRAIN_PARTICLE.get(), s -> new LodestoneTerrainParticleType.Factory());
         event.registerSpriteSet(ITEM_PARTICLE.get(), s -> new LodestoneItemCrumbsParticleType.Factory());
     }
