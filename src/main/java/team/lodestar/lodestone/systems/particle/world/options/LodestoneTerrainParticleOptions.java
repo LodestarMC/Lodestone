f02924fac33c6653adfecc319c45f82182fdecdf
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2024-06-04 05:48:25 +0200

    Killing someone

--- src/main/java/team/lodestar/lodestone/systems/particle/world/options/LodestoneTerrainParticleOptions.java
+++ src/main/java/team/lodestar/lodestone/systems/particle/world/options/LodestoneTerrainParticleOptions.java
@@ -12,21 +12,21 @@ public class LodestoneTerrainParticleOptions extends WorldParticleOptions {
     public final BlockState blockState;
     public final BlockPos blockPos;
 
-    public LodestoneTerrainParticleOptions(Supplier<ParticleType<LodestoneTerrainParticleOptions>> type, LodestoneParticleBehavior behavior, BlockState blockState, BlockPos blockPos) {
+    public LodestoneTerrainParticleOptions(ParticleType<LodestoneTerrainParticleOptions> type, LodestoneParticleBehavior behavior, BlockState blockState, BlockPos blockPos) {
         super(type, behavior);
         this.blockState = blockState;
         this.blockPos = blockPos;
     }
 
-    public LodestoneTerrainParticleOptions(Supplier<ParticleType<LodestoneTerrainParticleOptions>> type, BlockState blockState, BlockPos blockPos) {
+    public LodestoneTerrainParticleOptions(ParticleType<LodestoneTerrainParticleOptions> type, BlockState blockState, BlockPos blockPos) {
         this(type, null, blockState, blockPos);
     }
 
-    public LodestoneTerrainParticleOptions(Supplier<ParticleType<LodestoneTerrainParticleOptions>> type, LodestoneParticleBehavior behavior, BlockState blockState) {
+    public LodestoneTerrainParticleOptions(ParticleType<LodestoneTerrainParticleOptions> type, LodestoneParticleBehavior behavior, BlockState blockState) {
         this(type, behavior, blockState, null);
     }
 
-    public LodestoneTerrainParticleOptions(Supplier<ParticleType<LodestoneTerrainParticleOptions>> type, BlockState blockState) {
+    public LodestoneTerrainParticleOptions(ParticleType<LodestoneTerrainParticleOptions> type, BlockState blockState) {
         this(type, blockState, null);
     }
 }
