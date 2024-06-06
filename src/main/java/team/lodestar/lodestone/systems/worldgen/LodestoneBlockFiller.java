ec2b4f6b497591f118863808f17020828bf370b0
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2024-06-06 16:10:50 +0200

    Update LodestoneBlockFiller.java

--- src/main/java/team/lodestar/lodestone/systems/worldgen/LodestoneBlockFiller.java
+++ src/main/java/team/lodestar/lodestone/systems/worldgen/LodestoneBlockFiller.java
@@ -116,15 +116,16 @@ public class LodestoneBlockFiller extends ArrayList<LodestoneBlockFiller.Lodesto
         boolean canPlace(LevelAccessor level, BlockPos pos, BlockState state);
     }
 
-    public static BlockStateEntryBuilder build(BlockState state) {
+    public static BlockStateEntryBuilder create(BlockState state) {
         return new BlockStateEntryBuilder(state);
     }
 
     public static class BlockStateEntryBuilder {
-        private final BlockState state;
 
+        private final BlockState state;
         private EntryDiscardPredicate discardPredicate;
         private EntryPlacementPredicate placementPredicate;
+        private boolean isCareful;
 
         public BlockStateEntryBuilder(BlockState state) {
             this.state = state;
@@ -140,8 +141,17 @@ public class LodestoneBlockFiller extends ArrayList<LodestoneBlockFiller.Lodesto
             return this;
         }
 
+        public BlockStateEntryBuilder setCareful() {
+            return setCareful(true);
+        }
+
+        public BlockStateEntryBuilder setCareful(boolean isCareful) {
+            this.isCareful = isCareful;
+            return this;
+        }
+
         public BlockStateEntry build() {
-            return new BlockStateEntry(state, discardPredicate, placementPredicate);
+            return new BlockStateEntry(state, discardPredicate, placementPredicate, isCareful);
         }
     }
 
@@ -149,11 +159,13 @@ public class LodestoneBlockFiller extends ArrayList<LodestoneBlockFiller.Lodesto
         private final BlockState state;
         private final EntryDiscardPredicate discardPredicate;
         private final EntryPlacementPredicate placementPredicate;
+        private final boolean isCareful;
 
-        private BlockStateEntry(BlockState state, EntryDiscardPredicate discardPredicate, EntryPlacementPredicate placementPredicate) {
+        private BlockStateEntry(BlockState state, EntryDiscardPredicate discardPredicate, EntryPlacementPredicate placementPredicate, boolean isCareful) {
             this.state = state;
             this.discardPredicate = discardPredicate;
             this.placementPredicate = placementPredicate;
+            this.isCareful = isCareful;
         }
 
         public BlockState getState() {
@@ -171,6 +183,9 @@ public class LodestoneBlockFiller extends ArrayList<LodestoneBlockFiller.Lodesto
             BlockState state = level.getBlockState(pos);
             if (placementPredicate != null && !placementPredicate.canPlace(level, pos, state)) {
                 return false;
+            }
+            if (!isCareful) {
+                return true;
             }
             return level.isEmptyBlock(pos) || state.canBeReplaced();
         }
