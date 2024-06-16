a4997c01762283f4734fdf82edb1ab843c84e1d6
Cryptic <117136390+Its-Cryptic@users.noreply.github.com>
2024-06-15 18:06:35 -0700

    match forge

--- src/main/java/team/lodestar/lodestone/systems/model/obj/Face.java
+++ src/main/java/team/lodestar/lodestone/systems/model/obj/Face.java
@@ -2,6 +2,9 @@ package team.lodestar.lodestone.systems.model.obj;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.world.phys.Vec2;
 import org.joml.Matrix3f;
@@ -11,43 +14,35 @@ import org.joml.Vector3f;
 import java.util.List;
 import java.util.Locale;
 
-public record Face(List<Vector3f> vertices, List<Vector3f> normals, List<Vec2> uvs) {
-    public void renderFace(PoseStack poseStack, VertexConsumer buffer, int packedLight) {
-        if (vertices.size() == 3) {
-            renderTriangle(poseStack, buffer, packedLight);
-        } else if (vertices.size() == 4) {
-            renderQuad(poseStack, buffer, packedLight);
-        } else {
-            throw new RuntimeException("Face has invalid number of vertices. Supported vertex counts are 3 and 4.");
-        }
+public record Face(List<Vertex> vertices) {
+    public void renderFace(PoseStack poseStack, RenderType renderType, int packedLight) {
+        MultiBufferSource.BufferSource mcBufferSource = Minecraft.getInstance().renderBuffers().bufferSource();
+        //MultiBufferSource.BufferSource lodestoneBufferSource = RenderHandler.DELAYED_RENDER.getTarget();
+        VertexConsumer buffer = mcBufferSource.getBuffer(renderType);
+        int vertexCount = vertices.size();
+
+        if (vertexCount == 4) renderQuad(poseStack, buffer, packedLight);
+        else if (vertexCount == 3) renderTriangle(poseStack, buffer, packedLight);
+        else throw new RuntimeException("Face has invalid number of vertices. Supported vertex counts are 3 and 4.");
     }
     public void renderTriangle(PoseStack poseStack, VertexConsumer buffer, int packedLight) {
-        Matrix4f matrix4f = poseStack.last().pose();
-        Matrix3f normalMatrix = poseStack.last().normal();
-        List<Vector3f> vertices = this.vertices();
-        List<Vector3f> normals = this.normals();
-        List<Vec2> uvs = this.uvs();
-
-        for (int i = 0; i < 3; i++) {
-            addVertex(buffer, matrix4f, vertices.get(i), uvs.get(i), normalMatrix, normals.get(i), packedLight);
-        }
-        addVertex(buffer, matrix4f, vertices.get(0), uvs.get(0), normalMatrix, normals.get(0), packedLight);
+        this.vertices().forEach(vertex -> addVertex(buffer, vertex, poseStack, packedLight));
+        addVertex(buffer, this.vertices().get(0), poseStack, packedLight);
     }
 
     public void renderQuad(PoseStack poseStack, VertexConsumer buffer, int packedLight) {
-        Matrix4f matrix4f = poseStack.last().pose();
-        Matrix3f normalMatrix = poseStack.last().normal();
-        List<Vector3f> vertices = this.vertices();
-        List<Vector3f> normal = this.normals();
-        List<Vec2> uvs = this.uvs();
-
-        for (int i = 0; i < 4; i++) {
-            addVertex(buffer, matrix4f, vertices.get(i), uvs.get(i), normalMatrix, normals.get(i), packedLight);
-        }
+        this.vertices().forEach(vertex -> addVertex(buffer, vertex, poseStack, packedLight));
     }
 
-    private void addVertex(VertexConsumer buffer, Matrix4f matrix4f, Vector3f vertex, Vec2 uv, Matrix3f normalMatrix, Vector3f normal, int packedLight) {
-        buffer.vertex(matrix4f, vertex.x(), vertex.y(), vertex.z())
+    private void addVertex(VertexConsumer buffer, Vertex vertex, PoseStack poseStack, int packedLight) {
+        Matrix4f matrix4f = poseStack.last().pose();
+        Matrix3f normalMatrix = poseStack.last().normal();
+
+        Vector3f position = vertex.position();
+        Vector3f normal = vertex.normal();
+        Vec2 uv = vertex.uv();
+
+        buffer.vertex(matrix4f, position.x(), position.y(), position.z())
                 .color(255, 255, 255, 255)
                 .uv(uv.x, -uv.y)
                 .overlayCoords(OverlayTexture.NO_OVERLAY)
@@ -58,8 +53,8 @@ public record Face(List<Vector3f> vertices, List<Vector3f> normals, List<Vec2> u
 
     public Vector3f getCentroid() {
         Vector3f centroid = new Vector3f();
-        for (Vector3f vertex : vertices) {
-            centroid.add(vertex);
+        for (Vertex vertex : vertices) {
+            centroid.add(vertex.position());
         }
         centroid.div(vertices.size());
         return centroid;
