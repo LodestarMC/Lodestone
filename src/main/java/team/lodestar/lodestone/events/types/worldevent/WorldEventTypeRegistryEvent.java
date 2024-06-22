a97306903ee9241ff50a1b7e0f270c0a50dcea24
mrsterner <simon@sterner.dev>
2024-06-22 16:58:53 +0200

    stuff

--- src/main/java/team/lodestar/lodestone/events/types/worldevent/WorldEventTypeRegistryEvent.java
+++ src/main/java/team/lodestar/lodestone/events/types/worldevent/WorldEventTypeRegistryEvent.java
@@ -1,10 +1,10 @@
 package team.lodestar.lodestone.events.types.worldevent;
-
+/*TODO
 import net.minecraft.resources.ResourceLocation;
 import team.lodestar.lodestone.LodestoneLib;
 import team.lodestar.lodestone.registry.common.LodestoneWorldEventTypeRegistry;
 import team.lodestar.lodestone.systems.worldevent.WorldEventType;
-/*TODO
+
 public class WorldEventTypeRegistryEvent extends Event implements IModBusEvent {
     public WorldEventType create(ResourceLocation id, WorldEventType.EventInstanceSupplier instanceSupplier) {
         LodestoneLib.LOGGER.info("Registering world event type: " + id);
