cc7c21e049841d1e134bffdfd6b658e7bca70efb
mrsterner <simon@sterner.dev>
2024-07-06 16:32:14 +0200

    neogrog

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,35 +42,35 @@ neoForge {
     // Default run configurations.
     // These can be tweaked, removed, or duplicated as needed.
     runs {
-        client {
+        create("client") {
             client()
 
             // Comma-separated list of namespaces to load gametests from. Empty = all namespaces.
-            systemProperty('neoforge.enabledGameTestNamespaces', "${property("mod_id")}")
+            systemProperty("neoforge.enabledGameTestNamespaces", "${property("mod_id")}")
         }
 
-        server {
+        create("server") {
             server()
-            programArgument('--nogui')
-            systemProperty('neoforge.enabledGameTestNamespaces', "${property("mod_id")}")
+            programArgument("--nogui")
+            systemProperty("neoforge.enabledGameTestNamespaces", "${property("mod_id")}")
         }
 
         // This run config launches GameTestServer and runs all registered gametests, then exits.
         // By default, the server will crash when no gametests are provided.
         // The gametest system is also enabled by default for other run configs under the /test command.
-        gameTestServer {
+        create("gameTestServer") {
             type = "gameTestServer"
-            systemProperty('neoforge.enabledGameTestNamespaces', "${property("mod_id")}")
+            systemProperty("neoforge.enabledGameTestNamespaces", "${property("mod_id")}")
         }
 
-        data {
+        create("data") {
             data()
 
             // example of overriding the workingDirectory set in configureEach above, uncomment if you want to use it
             // gameDirectory = project.file('run-data')
 
             // Specify the modid for data generation, where to output the resulting resource, and where to look for existing resources.
-            programArguments.addAll('--mod', project.mod_id, '--all', '--output', file('src/generated/resources/').getAbsolutePath(), '--existing', file('src/main/resources/').getAbsolutePath())
+            programArguments.addAll("--mod", "${property("mod_id")}", "--all", "--output", file("src/generated/resources/").getAbsolutePath(), "--existing", file("src/main/resources/").getAbsolutePath())
         }
 
         // applies to all the run configs above
@@ -80,7 +80,7 @@ neoForge {
             // "SCAN": For mods scan.
             // "REGISTRIES": For firing of registry events.
             // "REGISTRYDUMP": For getting the contents of all registries.
-            systemProperty('forge.logging.markers', 'REGISTRIES')
+            systemProperty("forge.logging.markers", "REGISTRIES")
 
             // Recommended logging level for the console
             // You can set various levels here.
@@ -94,8 +94,9 @@ neoForge {
         // these are used to tell the game which sources are for which mod
         // mostly optional in a single mod project
         // but multi mod projects should define one per mod
-        "${mod_id}" {
-            sourceSet(sourceSets.main)
+        create("${property("mod_id")}") {
+            //sourceSet(sourceSets.main)
+            //modSource(property("mod_id").sourceSets["main"])
         }
     }
 }
@@ -138,20 +139,21 @@ repositories {
 }
 
 dependencies {
-    minecraft("net.minecraftforge:forge:${minecraftVersion}-${forgeVersion}")
+    //minecraft("net.minecraftforge:forge:${minecraftVersion}-${forgeVersion}")
 
-    if (System.getProperty("idea.sync.active") != "true") {
-        annotationProcessor("org.spongepowered:mixin:${mixinVersion}:processor")
-    }
+    //if (System.getProperty("idea.sync.active") != "true") {
+    //    annotationProcessor("org.spongepowered:mixin:${mixinVersion}:processor")
+    //}
 
     // JEI Dependency
 //    compileOnly(fg.deobf("mezz.jei:jei-${minecraftVersion}-forge-api:${jeiVersion}"))
 //    compileOnly(fg.deobf("mezz.jei:jei-${minecraftVersion}-common-api:${jeiVersion}"))
-    runtimeOnly(fg.deobf("mezz.jei:jei-${minecraftVersion}-forge:${jeiVersion}"))
+    //runtimeOnly(fg.deobf("mezz.jei:jei-${minecraftVersion}-forge:${jeiVersion}"))
+    runtimeOnly("mezz.jei:jei-${property("minecraft_version")}-neoforge:${property("jei_version")}")
 
     // Curios dependency
-    compileOnly(fg.deobf("top.theillusivec4.curios:curios-forge:${curiosVersion}:api"))
-    runtimeOnly(fg.deobf("top.theillusivec4.curios:curios-forge:${curiosVersion}"))
+    compileOnly("top.theillusivec4.curios:curios-neoforge:${property("curios_version")}:api")
+    runtimeOnly("top.theillusivec4.curios:curios-neoforge:${property("curios_version")}")
 
 //    implementation(fg.deobf("com.sammy.malum:malum:${minecraftVersion}-1.6.72"))
 }
@@ -162,17 +164,17 @@ tasks.withType<ProcessResources> {
     filesMatching(listOf("META-INF/mods.toml", "pack.mcmeta")) {
         expand(
                 mapOf(
-                        "forgeVersionRange" to forgeVersionRange,
-                        "loaderVersionRange" to loaderVersionRange,
-                        "minecraftVersion" to minecraftVersion,
-                        "minecraftVersionRange" to minecraftVersionRange,
-                        "modAuthors" to modAuthors,
-                        "modDescription" to modDescription,
-                        "modId" to modId,
-                        "modJavaVersion" to modJavaVersion,
-                        "modName" to modName,
-                        "modVersion" to version,
-                        "modLicense" to modLicense
+                        "neo_version_range" to "${property("neo_version_range")}",
+                        "loader_version_range" to "${property("loader_version_range")}",
+                        "minecraft_version" to "${property("minecraft_version")}",
+                        "minecraft_version_range" to "${property("minecraft_version_range")}",
+                        "mod_authors" to "${property("mod_authors")}",
+                        "mod_description" to "${property("mod_description")}",
+                        "mod_id" to "${property("mod_id")}",
+                        "mod_java_version" to "${property("mod_java_version")}",
+                        "mod_name" to "${property("mod_name")}",
+                        "mod_version" to "${property("version")}",
+                        "mod_license" to "${property("mod_license")}"
                 )
         )
     }
@@ -182,29 +184,30 @@ java {
     withJavadocJar()
     withSourcesJar()
 }
-
+/*
 tasks.withType<Jar> {
     val now = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(Date())
     manifest {
         attributes(mapOf(
-                "Specification-Title" to modName,
-                "Specification-Vendor" to modAuthors,
+                "Specification-Title" to "${property("mod_name")}",
+                "Specification-Vendor" to "${property("mod_authors")}",
                 "Specification-Version" to '1',
-                "Implementation-Title" to modName,
-                "Implementation-Version" to version,
-                "Implementation-Vendor" to modAuthors,
+                "Implementation-Title" to "${property("mod_name")}",
+                "Implementation-Version" to "${property("version")}",
+                "Implementation-Vendor" to "${property("mod_authors")}",
                 "Implementation-Timestamp" to now,
         ))
     }
     finalizedBy("reobfJar")
 }
 
+ */
+
 publishing {
     publications {
         register<MavenPublication>("mavenJava") {
             artifactId = baseArchivesName
             from(components["java"])
-            fg.component(this)
         }
     }
     repositories {
